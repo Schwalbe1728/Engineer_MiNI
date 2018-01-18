04d2d0c42d235a4cbde0668903f3b7a4312c2175
Jacek Schwalbe Staszewski <schwalbe1728@gmail.com>
2018-01-18 19:19:32 +0100

    Added plot screenshot and plot hide/show toggle functionalities

---
+++ Car Simulation/Assets/Scripts/UI/ScreenShotScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class ScreenShotScript : MonoBehaviour {
+    
+    private RectTransform UIPanelToCapture;
+
+    [SerializeField]
+    private string targetPath;
+
+    private static uint ScreenShotCount = 0;
+
+    void Awake()
+    {
+        UIPanelToCapture = gameObject.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            StartCoroutine(TakeScreenshot());
+        }
+    }
+
+    private IEnumerator TakeScreenshot()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Vector2 temp = UIPanelToCapture.transform.position;
+        var startX = temp.x - UIPanelToCapture.rect.width / 2;
+        var startY = temp.y - UIPanelToCapture.rect.height / 2;
+
+        int w = System.Convert.ToInt32(UIPanelToCapture.rect.width);
+        int h = System.Convert.ToInt32(UIPanelToCapture.rect.height);
+
+        Texture2D texture = new Texture2D(w, h, TextureFormat.RGB24, false);
+        texture.ReadPixels(
+            new Rect(
+                startX, 
+                startY, 
+                UIPanelToCapture.rect.width, 
+                UIPanelToCapture.rect.height), 
+            0, 0);
+
+        texture.Apply();        
+
+        var bytes = texture.EncodeToPNG();
+        Destroy(texture);
+
+        string basePath = Application.dataPath + targetPath;
+        string tryPath = null;
+
+        while(File.Exists( (tryPath = basePath + "Screenshot" + ScreenShotCount.ToString() + " " + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png" ) ))
+        {
+            ScreenShotCount++;
+        }
+
+        yield return null;
+
+        File.WriteAllBytes(tryPath, bytes);
+    }
+}
---
+++ Car Simulation/Assets/Scripts/UI/StatsToggles.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsToggles : MonoBehaviour
+{
+    [SerializeField]
+    List<KeyToPanelBinding> Bindings;
+
+    void Update()
+    {
+        foreach(KeyToPanelBinding bind in Bindings)
+        {
+            if(Input.GetKeyDown(bind.Key))
+            {
+                bind.Toggle();
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class KeyToPanelBinding
+{
+    public KeyCode Key;
+    public GameObject Panel;
+    private bool Shown = true;
+
+    public void Toggle()
+    {
+        Shown = !Shown;
+        Panel.SetActive(Shown);
+    }
+}
